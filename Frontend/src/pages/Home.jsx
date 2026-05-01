79ef34b106e2e34489ef5695913f7c37d5092ece
Keerthana-naik <keerthananaik64@gmail.com>
2026-05-01 12:34:47 +0530

    7

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -18,9 +18,10 @@ function Home() {
 
     axios
       .get(url)
-      .then((res) => {
+      .then((res) => 
+      {
       console.log(res.data);
-      setProducts(res.data.products);
+      setProducts(res.data);
   })
       .catch((err) => console.log(err));
   }, [selectedCategory]);
@@ -99,8 +100,7 @@ function Home() {
         </div>
 
         <div className="bestgrid">
-          {/* {products.map((product) => ( */}
-          {(products || []).map((product) => (
+          {products.map((product) => (
             <div key={product._id}
               onClick={() => navigate(`/product/${product._id}`)}
             >
